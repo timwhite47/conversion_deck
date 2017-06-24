1a3407ad1724a0861e5c69e4e3343fa0691235db
Tim White <tim@timwhite.ninja>
2017-06-23 22:09:36 -0700

    try using dynamodb

--- src/analytics.py
+++ src/analytics.py
@@ -1,16 +1,21 @@
 import requests
 import json
+import boto3
 from urllib import urlencode
 from datetime import date, timedelta
 from os import environ
 from requests.auth import HTTPBasicAuth
 
+TABLE_NAME = 'conversion_deck.events'
 BASE_URL = 'https://data.mixpanel.com/api/2.0/export'
 
 class Analytics(object):
     """Data from Mixpanel"""
     def __init__(self):
+        dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
         self.token = environ['HD_MIXPANEL_TOKEN']
+        self.table = dynamodb.Table(TABLE_NAME)
 
     def fetch(self, start_date, end_date):
         increment = timedelta(days=1)
@@ -20,14 +25,22 @@ class Analytics(object):
 
         while from_date <= end_date:
             url = self._generate_url(from_date, to_date)
+
             print "Fetching URL: {}".format(url)
             response = requests.get(url, auth=auth)
-            data = self._parse_response(response)
 
-            import ipdb; ipdb.set_trace()
+            event_data = self._parse_response(response)
+            [self._store_event(event) for event in event_data]
+
+            print "{} events stored".format(len(event_data))
 
             from_date = to_date
             to_date = from_date + increment
+
+    def _store_event(self, event):
+        return self.table.put_item(
+           Item=event
+        )
 
     def _parse_response(self, response):
         return response.text.split('\n')
