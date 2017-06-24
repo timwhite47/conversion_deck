71c0d3c867e5a2f2c9ab1e54b5e993b3950d4c1f
Tim White <tim@timwhite.ninja>
2017-06-23 23:33:28 -0700

    add multiprocessing

--- src/analytics.py
+++ src/analytics.py
@@ -2,7 +2,7 @@ import requests
 import json
 import boto3
 import botocore
-
+from multiprocessing import cpu_count, Pool
 from urllib import urlencode
 from datetime import date, timedelta
 from os import environ
@@ -10,21 +10,41 @@ from requests.auth import HTTPBasicAuth
 
 TABLE_NAME = 'conversion_deck.events'
 BASE_URL = 'https://data.mixpanel.com/api/2.0/export'
+TABLE = dynamodb.Table(TABLE_NAME)
+def _store_event(self, event):
+    try:
+        return TABLE.put_item(Item=event)
+    except botocore.exceptions.ClientError as e:
+        print "Could not store event"
+        print event
+        print e
+        print '='*20
+    except Exception as e:
+        print "Could not store event"
+        print event
+        print e
+        print '='*20
+        import ipdb; ipdb.set_trace()
 
 class Analytics(object):
     """Data from Mixpanel"""
     def __init__(self):
         dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-
         self.token = environ['HD_MIXPANEL_TOKEN']
-        self.table = dynamodb.Table(TABLE_NAME)
+        
+        try:
+            cpus = multiprocessing.cpu_count()
+        except NotImplementedError:
+            cpus = 4
+
+        self.cpus = cpus
 
     def fetch(self, start_date, end_date):
         increment = timedelta(days=1)
         from_date = start_date
         to_date = start_date + increment
         auth = HTTPBasicAuth(self.token, '')
-
+        pool = Pool(processes=self.cpus)
         while from_date <= end_date:
             url = self._generate_url(from_date, to_date)
 
@@ -32,23 +52,11 @@ class Analytics(object):
             response = requests.get(url, auth=auth)
 
             event_data = self._parse_response(response)
-            [self._store_event(event) for event in event_data]
+            pool.map(_store_event, event_data)
             print "{} events stored".format(len(event_data))
 
             from_date = to_date
             to_date = from_date + increment
-
-    def _store_event(self, event):
-        try:
-            return self.table.put_item(
-               Item=event
-            )
-        except botocore.exceptions.ClientError as e:
-            print "Could not store event"
-            print event
-            print e
-        except Exception as e:
-            import ipdb; ipdb.set_trace()
 
 
     def _parse_entry(self, entry):
