9088bd88a2a65a5826b5fd9b07dcb8aa4ed0c6f6
Tim White <tim@timwhite.ninja>
2017-06-28 20:28:31 -0700

    Limit events and profiles to the last 90 days

--- src/analytics.py
+++ src/analytics.py
@@ -3,6 +3,7 @@ import json
 import boto3
 import botocore
 import pandas as pd
+from time import sleep
 from db import create_event, create_profile
 from urllib import urlencode
 from datetime import date, timedelta
@@ -11,6 +12,7 @@ from requests.auth import HTTPBasicAuth
 
 EXPORT_URL = 'https://data.mixpanel.com/api/2.0/export'
 ENGAGE_URL = 'https://mixpanel.com/api/2.0/engage'
+TIMEFRAME_DAYS = 90
 
 with open('data/event_names.json') as json_file:
     data = json.load(json_file)
@@ -21,22 +23,30 @@ class Analytics(object):
     def __init__(self, token):
         self.token = token
 
-    def fetch_events(self, days=30, increment=1):
+    def fetch_events(self, days=TIMEFRAME_DAYS, increment=1):
         for num_days in xrange(1, days + 1):
             end_date = date.today() - timedelta(days=num_days)
             start_date = end_date - timedelta(days=increment)
             url = self._generate_export_url(start_date, end_date)
-            self._fetch_url(url)
+            print "Fetching for {}".format(str(start_date))
+            self._export_events(url)
+
+            # Be a nice API citizen, sleep for 1 minute
+            print "Page {} finished, sleeping for 2 minutes"
+            sleep(60)
 
     def fetch_people(self):
         page=0
         auth = HTTPBasicAuth(self.token, '')
         session_id = ''
+        initial_url = self._generate_engage_url()
+        session_id = self._fetch_url(initial_url).json()['session_id']
+
         while True:
             print 'Getting page: {}'.format(page)
 
             url = self._generate_engage_url(page, session_id)
-            response = requests.get(url, auth=auth)
+            response = self._fetch_url(url)
             data = response.json()
             if data['session_id']:
                 session_id = data['session_id']
@@ -47,10 +57,12 @@ class Analytics(object):
 
         return True
 
-    def _fetch_url(self, url):
-        print "Fetching URL: {}".format(url)
+    def _fetch_url(self, url, stream=False):
         auth = HTTPBasicAuth(self.token, '')
-        response = requests.get(url, auth=auth, stream=True)
+        return requests.get(url, auth=auth, stream=stream)
+
+    def _export_events(self, url):
+        response = self._fetch_url(url, stream=True)
 
         try:
             for line in response.iter_lines():
@@ -72,16 +84,24 @@ class Analytics(object):
         return '?'.join([EXPORT_URL, params])
 
     def _generate_engage_url(self, page=0, session_id=''):
-        params = urlencode({
+        behaviors = [{
+            'event_selectors': [{'event': 'Sign Up'}],
+            'name': 'signup',
+            'window': '{}d'.format(TIMEFRAME_DAYS)
+        }]
+
+        selector = '(behaviors["signup"] > 0)'
+
+        params = {
             'page': page,
-            'session_id': session_id
-        })
-        return '?'.join([ENGAGE_URL, params])
+            'session_id': session_id,
+            'behaviors': json.dumps(behaviors),
+            'selector': selector
+        }
 
-if __name__ == '__main__':
-    a = Analytics()
-    timeframe = timedelta(days=7)
+        if len(session_id) == 0:
+            del params['session_id']
+            del params['behaviors']
+            del params['selector']
 
-    end_date = date.today()
-    start_date = end_date - timeframe
-    a.fetch(start_date, end_date)
+        return '?'.join([ENGAGE_URL, urlencode(params)])
